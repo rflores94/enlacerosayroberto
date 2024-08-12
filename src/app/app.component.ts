dc1f5fe14533d30cd3e175dc6da207ea428913a9
roberto.flores <roberto.flores@onizea.com>
2024-08-12 13:10:14 +0200

    padding y menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Bienvenidos a nuestra web de boda.';
   date = '7 de diciembre del 2024';
+
+  // Método para cerrar el menú
+  closeMenu(): void {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse) {
+      navbarCollapse.classList.remove('show');
+    }
+  }
 }
